0be1c0cee93c96d1b276ec0fa8929e0a0a0e7056
inmenton <vladiki23@icloud.com>
2025-04-14 20:35:30 +0200

    upp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -28,6 +29,7 @@ export default function RootLayout({
                 <main className="flex-grow pt-8">{children}</main>
                 <Footer/>
                 <Analytics />
+                <SpeedInsights/>
             </div>
         </ThemeProvider>
         </body>
