511d960e966f70d91a3c6474c7618be421ef18b1
inmenton <vladiki23@icloud.com>
2025-04-15 13:21:59 +0200

    upp

--- src/components/LicenseSection.tsx
+++ src/components/LicenseSection.tsx
@@ -13,15 +13,15 @@ const logos = [
 export default function LicenseSection() {
     return (
         <section className="py-10 px-4 max-w-4xl mx-auto mt-12">
-            <div className="flex flex-col gap-4 justify-center">
+            <div className="flex flex-col gap-4 justify-center items-center">
                 {/* Лицензии */}
-                <div className="w-full md:w-[270px] border border-neutral-700 rounded-xl p-4">
-                    <h2 className=" font-semibold mb-1 text-sm">Fully Licensed Pilot</h2>
-                    <p className="text-xs text-gray-400 mb-3 ">
+                <div className="w-full md:w-[270px] border border-neutral-700 rounded-xl p-4 text-center mx-auto">
+                    <h2 className="font-semibold mb-1 text-sm">Fully Licensed Pilot</h2>
+                    <p className="text-xs text-gray-400">
                         Based in London. Licensed in the UK and EU.
                     </p>
 
-                    <div className="grid grid-cols-2 justify-items-center">
+                    <div className="grid grid-cols-2 gap-3 sm:gap-4 mt-3 justify-items-center">
                         {logos.map((src, index) => (
                             <div key={index} className="w-[100px] h-[70px]">
                                 <Image
@@ -37,7 +37,8 @@ export default function LicenseSection() {
                 </div>
 
                 {/* Контакты */}
-                <div className="align-items-center w-full md:w-[270px] border border-neutral-700 rounded-xl p-4 flex flex-col align-imtems-center">
+                <div
+                    className="align-items-center w-full md:w-[270px] border border-neutral-700 rounded-xl p-4 flex flex-col align-imtems-center">
                     <h3 className="text-base font-semibold mb-6">Contact</h3>
 
                     <div className="text-sm space-y-2">
@@ -47,13 +48,13 @@ export default function LicenseSection() {
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            <Phone size={16} /> +4474 939 12 469
+                            <Phone size={16}/> +4474 939 12 469
                         </a>
                         <a
                             href="mailto:pro.dronepilot.uk@gmail.com"
                             className="flex items-center gap-2 hover:text-blue-400"
                         >
-                            <Mail size={16} /> pro.dronepilot.uk@gmail.com
+                            <Mail size={16}/> pro.dronepilot.uk@gmail.com
                         </a>
                         <a
                             href="https://www.instagram.com/dronefromlife?igsh=MTd0MncxM3RuOTlnMA%3D%3D&utm_source=qr"
@@ -61,7 +62,7 @@ export default function LicenseSection() {
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            <Instagram size={16} /> @dronefromlife
+                            <Instagram size={16}/> @dronefromlife
                         </a>
                     </div>
                 </div>
