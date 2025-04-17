07656ccdaf4071f02d851f036e0c75b540e59ffc
inmenton <vladiki23@icloud.com>
2025-04-17 11:14:58 +0200

    upp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,8 @@ import { ThemeProvider } from 'next-themes'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,19 +21,28 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
-        <body className={`${inter.className} bg-gray-50 text-gray-900 dark:bg-neutral-900 dark:text-white`}>
+        <head>
+            <meta
+                name="google-site-verification"
+                content="-Vk_-62e3r-OfrvCc_bG0e0g9-LYtfJawHUmWrY1p7w"
+            />
+        </head>
+        <body
+            className={`${inter.className} bg-gray-50 text-gray-900 dark:bg-neutral-900 dark:text-white`}
+        >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
             <div className="flex flex-col min-h-screen">
-                <Header/>
+                <Header />
                 <main className="flex-grow pt-8">{children}</main>
-                <Footer/>
+                <Footer />
                 <Analytics />
-                <SpeedInsights/>
+                <SpeedInsights />
             </div>
         </ThemeProvider>
         </body>
         </html>
     )
 }
+
 
 
